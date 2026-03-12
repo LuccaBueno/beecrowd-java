d58b8009e73a8e8e9ea06491ddef5b7cad9c5c9e
LuccaBueno <107876991+LuccaBueno@users.noreply.github.com>
2026-03-11 23:09:58 -0300

    Create 1014.java

---
+++ iniciante/1014.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    		int X = sc.nextInt();
+    		float Y = sc.nextFloat();
+    	
+    		System.out.printf("%.3f km/l\n", X/Y);
+ 
+    }
+ 
+}
