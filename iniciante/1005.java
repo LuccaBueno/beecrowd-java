a7142ea2b530804f63f0aa54e0786e4034fa925f
LuccaBueno <107876991+LuccaBueno@users.noreply.github.com>
2026-03-11 22:54:13 -0300

    Create 1005.java

---
+++ iniciante/1005.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+ 
+        Scanner sc = new Scanner(System.in);
+		    double A = sc.nextDouble();
+		    double B = sc.nextDouble();
+	    	double MEDIA = ((3.5 * A) + (7.5 * B))/11;
+		    System.out.printf("MEDIA = %.5f%n", MEDIA);
+ 
+    }
+ 
+}
