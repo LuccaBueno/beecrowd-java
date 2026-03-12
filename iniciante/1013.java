01cc37772b21b0c2af2648f8843d45fdc60752c3
LuccaBueno <107876991+LuccaBueno@users.noreply.github.com>
2026-03-11 23:09:04 -0300

    Create 1013.java

---
+++ iniciante/1013.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+         Scanner sc = new Scanner(System.in);
+      		int A = sc.nextInt();
+      		int B = sc.nextInt();
+      		int C = sc.nextInt();
+      		int maior = (A + B + Math.abs(A - B)) / 2;
+      		if (C>maior){
+      			maior = C;
+      		}
+      		System.out.printf("%d eh o maior\n", maior);
+ 
+    }
+ 
+}
