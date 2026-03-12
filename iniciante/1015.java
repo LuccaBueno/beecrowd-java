e2f345cfda1edb5b83a8ceb8dc6a41e42301552e
LuccaBueno <107876991+LuccaBueno@users.noreply.github.com>
2026-03-11 23:12:04 -0300

    Create 1015.java

---
+++ iniciante/1015.java
@@ -0,0 +1,14 @@
+ import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);;
+    		float x1 = sc.nextFloat();
+    		float y1 = sc.nextFloat();
+    		float x2 = sc.nextFloat();
+    		float y2 = sc.nextFloat();
+    		double res = Math.sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+    		System.out.printf("%.4f\n", res);
+    }
+ 
+}
