61d3fdbd542ad5d619cc7524d8c3e4b6be98f8df
LuccaBueno <107876991+LuccaBueno@users.noreply.github.com>
2026-03-11 22:51:34 -0300

    Create 1003.java

---
+++ iniciante/1003.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+ 
+      Scanner sc = new Scanner(System.in);
+		  int A = sc.nextInt();
+		  int B = sc.nextInt();
+		  int soma = A + B;
+	  	System.out.printf("SOMA = %d%n", soma);
+    }
+ 
+}
