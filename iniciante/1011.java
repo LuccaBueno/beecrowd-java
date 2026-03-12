5c5384411b512c1649a8ddde97c1f164225be096
LuccaBueno <107876991+LuccaBueno@users.noreply.github.com>
2026-03-11 23:06:58 -0300

    Create 1011.java

---
+++ iniciante/1011.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) {
+ 
+        Scanner sc = new Scanner(System.in);
+    		double Pi = 3.14159;
+    		double R = sc.nextDouble();
+    
+    		System.out.printf("VOLUME = %.3f%n", (4.0 / 3) * Pi * R * R * R);
+ 
+    }
+ 
+}
