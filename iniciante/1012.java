29442710f1619bcb556d32bdb2469333f4d13701
LuccaBueno <107876991+LuccaBueno@users.noreply.github.com>
2026-03-11 23:08:10 -0300

    Create 1012.java

---
+++ iniciante/1012.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+    		double Pi = 3.14159;
+    		double A = sc.nextDouble();
+    		double B = sc.nextDouble();
+    		double C = sc.nextDouble();
+    		
+    		System.out.printf("TRIANGULO: %.3f%nCIRCULO: %.3f%nTRAPEZIO: %.3f%nQUADRADO: %.3f%nRETANGULO: %.3f%n", (A*C)/2, Pi*C*C, (A+B) * C/2, B*B, A*B);
+        
+    }
+ 
+}
