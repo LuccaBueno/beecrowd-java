52565ccdd17881187644f9e60ac1f4efc7a90119
LuccaBueno <107876991+LuccaBueno@users.noreply.github.com>
2026-03-11 23:10:35 -0300

    Update 1012.java

--- iniciante/1012.java
+++ iniciante/1012.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
  
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
     		double Pi = 3.14159;
     		double A = sc.nextDouble();
