8705960e5952a59126116754976eaf881232cf7a
Lucca Bueno <107876991+LuccaBueno@users.noreply.github.com>
2026-03-14 19:37:31 -0300

    Create 1172.java

---
+++ arrays/1172.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    		int[] x = new int[10];
+    		for(int i = 0; i < 10; i++){
+    			x[i] = sc.nextInt();
+    		}
+    		for(int i = 0; i < 10; i++){
+    			if(x[i] <= 0){
+    				x[i] = 1;
+    			}
+    			System.out.printf("X[%d] = %d%n", i, x[i]);
+		}
+	}
+}
