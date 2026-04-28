0a7efa4801c6df47bc13604841ebf306d4fefdf4
Lucca Bueno <107876991+LuccaBueno@users.noreply.github.com>
2026-04-27 22:20:50 -0300

    Create 1183.java

---
+++ arrays_2D/1183.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		float[][] matriz = new float[12][12];
+		char B = sc.next().charAt(0);
+		float resultado = 0;
+		
+		for(int i = 0; i < 12; i++){
+			for(int j = 0; j < 12; j++){
+				
+				matriz[i][j] = sc.nextFloat();
+				
+				if(j > i){
+	
+					resultado += matriz[i][j];
+					
+				}
+			}
+		}
+		
+		if(B == 'S'){
+			System.out.printf("%.1f%n", resultado);
+		}else{
+			System.out.printf("%.1f%n", resultado/66);
+						
+		}
+	   
+    }
+}
