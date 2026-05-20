6e32067b39ea0dd02990462b6e3774006f53f2a5
Lucca Bueno <107876991+LuccaBueno@users.noreply.github.com>
2026-05-20 20:32:37 -0300

    Update 2552.java

--- arrays_2D/2552.java
+++ arrays_2D/2552.java
@@ -4,65 +4,69 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 		
-		
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-		
-		int[][] matriz = new int[N][M];
-		int[][] matriz2 = new int[N][M];
-		
-		for(int i = 0; i < matriz.length; i++){
-			for(int j = 0; j < matriz[i].length; j++){
-				
-				matriz[i][j] = sc.nextInt();
-				
-				
-			}
-		}
-		
-		
-		for(int i = 0; i < matriz.length; i++){
-			for(int j = 0; j < matriz[i].length; j++){
-				if(matriz[i][j] == 1){
+		while (sc.hasNextInt()) {
+			
+			int N = sc.nextInt();
+			int M = sc.nextInt();
+			
+			int[][] matriz = new int[N][M];
+			int[][] matriz2 = new int[N][M];
+			
+			for(int i = 0; i < matriz.length; i++){
+				for(int j = 0; j < matriz[i].length; j++){
 					
-					matriz2[i][j] = 9;
+					matriz[i][j] = sc.nextInt();
 					
 				}
-				
-				else{
-					int count = 0;
-					//cima
-					if(i - 1 >= 0 && matriz[i-1][j] != 0){
-						count++;
-					}
-					//baixo
-					if(i + 1 < matriz.length && matriz[i+1][j] != 0){
-						count++;
-					}
-					//esquerda
-					if(j - 1 >= 0 && matriz[i][j-1] != 0){
-						count++;
-					}
-					//direita
-					if(j + 1 < matriz[i].length && matriz[i][j+1] != 0){
-						count++;
+			}
+			
+			
+			for(int i = 0; i < matriz.length; i++){
+				for(int j = 0; j < matriz[i].length; j++){
+					
+					if(matriz[i][j] == 1){
+						
+						matriz2[i][j] = 9;
+						
 					}
 					
-					matriz2[i][j] = count;
+					else{
+						
+						int count = 0;
+						
+						//cima
+						if(i - 1 >= 0 && matriz[i-1][j] == 1){
+							count++;
+						}
+						
+						//baixo
+						if(i + 1 < matriz.length && matriz[i+1][j] == 1){
+							count++;
+						}
+						
+						//esquerda
+						if(j - 1 >= 0 && matriz[i][j-1] == 1){
+							count++;
+						}
+						
+						//direita
+						if(j + 1 < matriz[i].length && matriz[i][j+1] == 1){
+							count++;
+						}
+						
+						matriz2[i][j] = count;
 
+					}
+					
+					System.out.print(matriz2[i][j]);
+					
+					
+					
 				}
 				
-				System.out.print(matriz2[i][j]);
-				
-				
+				System.out.println();
 				
 			}
-			
-			System.out.println();
-			
 		}
-		
-		
-	   
     }
 }
