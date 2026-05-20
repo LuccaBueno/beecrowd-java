50e28b8501de60c5624e9f4982f97e7562864262
Lucca Bueno <107876991+LuccaBueno@users.noreply.github.com>
2026-05-20 18:03:53 -0300

    Create 2552.java

---
+++ arrays_2D/2552.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		
+		
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		int[][] matriz = new int[N][M];
+		int[][] matriz2 = new int[N][M];
+		
+		for(int i = 0; i < matriz.length; i++){
+			for(int j = 0; j < matriz[i].length; j++){
+				
+				matriz[i][j] = sc.nextInt();
+				
+				
+			}
+		}
+		
+		
+		for(int i = 0; i < matriz.length; i++){
+			for(int j = 0; j < matriz[i].length; j++){
+				if(matriz[i][j] == 1){
+					
+					matriz2[i][j] = 9;
+					
+				}
+				
+				else{
+					int count = 0;
+					//cima
+					if(i - 1 >= 0 && matriz[i-1][j] != 0){
+						count++;
+					}
+					//baixo
+					if(i + 1 < matriz.length && matriz[i+1][j] != 0){
+						count++;
+					}
+					//esquerda
+					if(j - 1 >= 0 && matriz[i][j-1] != 0){
+						count++;
+					}
+					//direita
+					if(j + 1 < matriz[i].length && matriz[i][j+1] != 0){
+						count++;
+					}
+					
+					matriz2[i][j] = count;
+
+				}
+				
+				System.out.print(matriz2[i][j]);
+				
+				
+				
+			}
+			
+			System.out.println();
+			
+		}
+		
+		
+	   
+    }
+}
